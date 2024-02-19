418ac35d5bb32e23c58e4d1fdbec45d7a0a09290
Brian Tambara <btambara@gmail.com>
2024-02-18 22:51:50 -0800

    add fetch for players url

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import PlayerSheet from "./components/PlayerSheet.vue";
+const pitchers = defineModel();
+
+fetch("http://localhost/api/v1/players/?position=1&skip=0").then(async response => {
+  const isJson = response.headers.get('content-type')?.includes('application/json');
+  const data = isJson && await response.json();
+  pitchers.value = data
+})
 </script>
 
 <template>
