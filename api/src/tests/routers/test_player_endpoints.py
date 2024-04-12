6dc5c8cb2289e679696ee7c14f5a790b965739d0
Brian Tambara <btambara@gmail.com>
2024-04-11 21:51:35 -0700

    add update and delete tests for player endpoints

--- api/src/tests/routers/test_player_endpoints.py
+++ api/src/tests/routers/test_player_endpoints.py
@@ -1,3 +1,4 @@
+import logging
 import random
 
 random_mlb_id = random.randint(100000, 999999)
@@ -52,6 +53,7 @@ def test_get_player_by_full_name_endpoint(test_client) -> None:
     assert response.status_code == 200
 
     response_json = response.json()
+    logging.getLogger(__name__).info(response_json)
     assert response_json["mlb_id"] == test_player["mlb_id"]
     assert response_json["full_name"] == test_player["full_name"]
     assert response_json["primary_number"] == test_player["primary_number"]
@@ -78,3 +80,29 @@ def test_get_player_by_primary_number_endpoint(test_client) -> None:
         response_json["primary_position_code"] == test_player["primary_position_code"]
     )
     assert response_json["stats"] == []
+
+
+def test_update_player_endpoint(test_client) -> None:
+    update_test_player = test_player.copy()
+    update_test_player["full_name"] = "New Test Name"
+
+    response = test_client.put("/api/v1/players/1", json=update_test_player)
+
+    assert response.status_code == 200
+
+    response_json = response.json()
+    assert response_json["mlb_id"] == update_test_player["mlb_id"]
+    assert response_json["full_name"] == update_test_player["full_name"]
+    assert response_json["primary_number"] == update_test_player["primary_number"]
+    assert response_json["current_team_id"] == update_test_player["current_team_id"]
+    assert (
+        response_json["primary_position_code"]
+        == update_test_player["primary_position_code"]
+    )
+    assert response_json["stats"] == []
+
+
+def test_delete_player_endpoint(test_client) -> None:
+    response = test_client.delete("/api/v1/players/1")
+
+    assert response.status_code == 200
